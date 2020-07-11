132218e9082e86df1cd240f5004186a82a2e476d
vbmavani <vbmavani003@gmail.com>
2020-07-11 21:49:38 +0530

    Fix Issue

--- src/services/index.js
+++ src/services/index.js
@@ -1,4 +1,4 @@
 exports.UserService = require('./userService.js');
 exports.passportService = require('./passportService');
 exports.TokenService = require('./tokenService');
-exports.PostService = require('./postService');
+exports.PostService = require('./postService'); 
